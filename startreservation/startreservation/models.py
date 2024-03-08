c78fd8a148ff1a9b8a6371ac69cf42f6f59e9fcb
Maxime <91160151+MaximePage@users.noreply.github.com>
2024-03-08 18:43:37 -0500

    Update models.py

--- startreservation/startreservation/models.py
+++ startreservation/startreservation/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class vehicles(models.Model):
     vehicle_id = models.IntegerField()
+
+class Reservation(models.Model):
+  Number = models.CharField(max_length=255)
+  
