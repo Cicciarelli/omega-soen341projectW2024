5fb9c2c0e412f5fd4df44128b6fc1d00d94c96dd
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2024-03-09 20:28:26 -0500

    Did The Renaming

--- omega/reservations/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Reservation(models.Model):
-    #vehicle_id = models.ForeignKey('vehicles.Vehicle')
-    reservation_start = models.DateTimeField()
-    reservation_end = models.DateTimeField()
-    reservation_price = models.DecimalField(decimal_places = 2, max_digits = 10)
