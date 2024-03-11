a8ad1d0793892e30aee15e14dd9dfb0750792124
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2024-03-11 18:08:16 -0400

    Edit Reservation Form

--- omega/omega/forms.py
+++ omega/omega/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from .models import Reservation
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['vehicle', 'reservation_start', 'reservation_end']
