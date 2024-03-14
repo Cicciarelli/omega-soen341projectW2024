01e5029e899bd130fa3893e8c0d11f961b83f6a1
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2024-03-14 14:01:46 -0400

    Using setUp now

--- omega/omega/tests.py
+++ omega/omega/tests.py
@@ -8,17 +8,35 @@ from django.shortcuts import redirect
 from . import views
 from django.urls import reverse
 import random
+from django.contrib.auth import get_user_model
 
 class TestExample(TestCase):
     def test_example(self):
         self.assertTrue(1==1)
 
 class ReservationTest(TestCase):
-    def test_create_reservation_with_valid_inputs(self):
-        vehicle = vehicles.objects.create(vehicle_id=0)
-        account = User.objects.create(username='test_user')
-        reservation_start = datetime.now()
-        reservation_end = reservation_start + timedelta(hours=1)
-    
-        reservation = Reservation(vehicle=vehicle, account=account, reservation_start=reservation_start, reservation_end=reservation_end)
-        self.assertIsInstance(reservation, Reservation)
+    def setUp(self):
+        # Create a user for testing
+        self.user = get_user_model().objects.create_user(
+            username='testuser', email='me@me.me', password='testpassword'
+        )
+        
+        # Create a vehicle for testing
+        self.vehicle = vehicles.objects.create(vehicle_id=0)
+
+        self.test_date = datetime.now()
+        
+        # Create a reservation for testing
+        self.reservation = Reservation.objects.create(
+            vehicle=self.vehicle,
+            account=self.user,
+            reservation_start=datetime.now(),
+            reservation_end = self.test_date + timedelta(hours=1)
+        )
+
+    def test_create_reservation_created(self):
+        self.assertEqual(self.reservation.vehicle, self.vehicle)
+        self.assertEqual(self.reservation.account, self.user)
+        self.assertEqual(self.reservation.reservation_start, self.test_date)
+        self.assertEqual(self.reservation.reservation_end, self.test_date + timedelta(hours=1))
+
