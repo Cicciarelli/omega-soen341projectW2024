dc31a32ba6368f0af885333e454bb0cefa2fb140
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2024-03-25 00:28:40 -0400

    Updated Reservation and Created Location Model

--- omega/omega/models.py
+++ omega/omega/models.py
@@ -11,14 +11,42 @@ class Vehicle(models.Model):
   vehicle_color = models.CharField(max_length=255)
   is_rented = models.BooleanField(null=False)
 
+  def __str__(self):
+        return self.vehicle_license_plate
+
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+
+class Location(models.Model):
+  title = models.CharField(max_length=255)
+
+  def __str__(self):
+        return self.title
 
 class Reservation(models.Model):
   vehicle = models.ForeignKey(Vehicle, on_delete=models.PROTECT) # Stops you from deleting a vehicle without first deleting the reservation
   account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # Deletes reservation if user account is deleted
   reservation_start = models.DateTimeField()
   reservation_end = models.DateTimeField()
-
-  
+  pick_up_location = models.ForeignKey(Location,
+                                       related_name='pick_up_reservation',
+                                       on_delete=models.CASCADE,
+                                         default=None,
+                                         blank=True,
+                                         null=True)
+  drop_off_location = models.ForeignKey(Location,
+                                        related_name='drop_off_location',
+                                        on_delete=models.CASCADE,
+                                         default=None,
+                                         blank=True,
+                                         null=True)
+  rental_period = models.DurationField(blank=True,
+                                       null=True)
+  mileage_limit = models.FloatField(blank=True,
+                                    null=True)
+  additional_services = models.CharField(max_length=255,
+                                         default=None,
+                                         blank=True,
+                                         null=True)
+  is_signed = models.BooleanField(default=False)
