445ed41eb76b435095b77039385f8747a80cb966
Kazemie <92108455+Kazemiee@users.noreply.github.com>
2024-03-25 23:56:01 -0400

    Temprotary Check Out View

--- omega/omega/views.py
+++ omega/omega/views.py
@@ -152,11 +152,10 @@ def checked_in_view(request, reservation_id):
     reservation.save()
     return redirect('checked_in', reservation_id=reservation_id)
 
-def checked_in_view(request, reservation_id):
+def check_out_view(request, reservation_id):
     reservation = get_object_or_404(Reservation, pk=reservation_id)
-    reservation.is_signed = True
-    reservation.save()
-    return redirect('checked_in', reservation_id=reservation_id)
+    # Write your view code here
+    raise Http404
 
 
 def vehicle_view(request):
